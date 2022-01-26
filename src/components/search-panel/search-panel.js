4c12843529889a60dd01060624dadb5b8286201e
Elena <elena.nisman85@gmail.com>
2022-01-26 19:43:19 +0200

    create component SeachPanel

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,11 @@
+
+
+const SeachPanel = () =>{
+  return (
+<input type="text"
+       className="form-control search-input" 
+       placeholder="Найти сотрудника"/>
+  )
+}
+
+export default SeachPanel;

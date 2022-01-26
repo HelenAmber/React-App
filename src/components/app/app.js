9b4ab4eb1d83d8852635880314df30ec126da144
Elena <elena.nisman85@gmail.com>
2022-01-26 19:51:51 +0200

    adding styles for search panel

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,13 @@
 import AppInfo from '../app-info/app-info';
 import './app.css';
+import SeachPanel from '../search-panel/search-panel';
 
 function App(){
     return (
         <div className="app">
            <AppInfo/>
            <div className='search-panel'>
-               
+               <SeachPanel/>
            </div>
         </div>
     );

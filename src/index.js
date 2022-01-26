96cf6105210856f5d56f04d818c21221bac239e8
Elena <elena.nisman85@gmail.com>
2022-01-26 18:14:46 +0200

    removing unnecessary components

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello, world!</h2>;
+
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
 
